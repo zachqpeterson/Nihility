013e9456bafe811cfa9c75a5ddec92dd69c48971
hababa2 <zactanpeterson@gmail.com>
2023-02-07 12:57:18 -0700

    Starting File I/O

--- Engine/Core/File.hpp
+++ Engine/Core/File.hpp
@@ -2,12 +2,22 @@
 
 #include "Defines.hpp"
 
+struct String;
+
 struct File
 {
 public:
+	File();
+	File(const String& path);
 
+	~File(); //calls close
+	void Close();
+
+	bool Opened() const;
 
 private:
 
-
+	//TODO: additional info
+	void* handle;
+	bool opened;
 };
