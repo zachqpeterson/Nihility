1359399a1836333bec7bfeb75ce00e7df3fae2a9
hababa2 <zactanpeterson@gmail.com>
2023-02-10 12:55:46 -0700

    Queue Start

--- Engine/Containers/Queue.hpp
+++ Engine/Containers/Queue.hpp
@@ -1,1 +1,34 @@
 #pragma once
+
+#include "Defines.hpp"
+
+template<typename T>
+struct Queue
+{
+public:
+	Queue();
+	Queue(U64 capacity);
+
+	~Queue();
+	void Destroy();
+
+	void Push(const T& value);
+	void Push(T&& value) noexcept;
+	const T& Peek() const;
+	void Pop(T& value);
+	void Pop(T&& value) noexcept;
+
+	const U64& Capacity() const;
+	const U64& Size() const;
+
+private:
+
+	U64 capacity;
+	U64 size;
+	T* array;
+
+	Queue(const Queue&) = delete;
+	Queue(Queue&&) = delete;
+	Queue& operator=(const Queue&) = delete;
+	Queue& operator=(Queue&&) = delete;
+};
