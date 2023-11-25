d8be182175b0a57c2aa787f4e4847c44683f3007
Zach Peterson <zactanpeterson@gmail.com>
2023-11-25 14:32:08 -0700

    Change VSync Through Settings

---
+++ Engine/Core/Events.cpp
@@ -0,0 +1,42 @@
+#include "Events.hpp"
+
+Hashmap<String, Events::Event, false> Events::events{ 128 };
+
+bool Events::Initialize()
+{
+	return true;
+}
+
+void Events::Shutdown()
+{
+	events.Destroy();
+}
+
+void Events::CreateEvent(const String& name)
+{
+	events.Insert(name, {});
+}
+
+void Events::ListenForEvent(const String& name, const Function<void()>& response)
+{
+	Event* event = events[name];
+
+	if (event) { event->listeners.Push(response); }
+}
+
+void Events::ListenForEvent(const String& name, Function<void()>&& response) noexcept
+{
+	Event* event = events[name];
+
+	if (event) { event->listeners.Push(Move(response)); }
+}
+
+void Events::Notify(const String& name)
+{
+	Event* event = events[name];
+
+	if (event)
+	{
+		for (Function<void()>& response : event->listeners) { response(); }
+	}
+}
